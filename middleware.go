79be4e35747a35d51c26f12da4d4a120b92cc9ee
morikuni <taihei.with+git@gmail.com>
2016-09-04 16:19:00 +0900

    Rename For to Apply

--- middleware.go
+++ middleware.go
@@ -10,12 +10,12 @@ type Middleware func(w http.ResponseWriter, r *http.Request, h http.HandlerFunc)
 // Compose composes two Middlewares.
 func (m Middleware) Compose(next Middleware) Middleware {
 	return func(w http.ResponseWriter, r *http.Request, h http.HandlerFunc) {
-		m(w, r, next.For(h))
+		m(w, r, next.Apply(h))
 	}
 }
 
-// For wraps a given http.HandlerFunc.
-func (m Middleware) For(h http.HandlerFunc) http.HandlerFunc {
+// Apply wraps a given http.HandlerFunc.
+func (m Middleware) Apply(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m(w, r, h)
 	}
