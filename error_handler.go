e2793191b124c2115caa0d30fc55e0be1c4769fc
morikuni <taihei.with+git@gmail.com>
2017-03-28 23:30:44 +0900

    Change ErrorHandler to interface

--- error_handler.go
+++ error_handler.go
@@ -4,6 +4,33 @@ import (
 	"net/http"
 )
 
-// ErrorHandler is a callback for a Service.
-// Called only when the Service returned a error.
-type ErrorHandler func(w http.ResponseWriter, r *http.Request, err error)
+type ErrorHandler interface {
+	HandleError(w http.ResponseWriter, r *http.Request, err error) error
+}
+
+type ErrorHandlerFunc func(w http.ResponseWriter, r *http.Request, err error) error
+
+func (f ErrorHandlerFunc) HandleError(w http.ResponseWriter, r *http.Request, err error) error {
+	return f(w, r, err)
+}
+
+type chainedErrorHandler []ErrorHandler
+
+func (c chainedErrorHandler) HandleError(w http.ResponseWriter, r *http.Request, err error) error {
+	for _, h := range c {
+		err = h.HandleError(w, r, err)
+		if err == nil {
+			return nil
+		}
+	}
+}
+
+func ComposeErrorHandler(handlers ...ErrorHandler) ErrorHandler {
+	l := len(handlers)
+	switch l {
+	case 0:
+		panic(ErrEmptyArgs)
+	default:
+		return chainedErrorHandler(handlers)
+	}
+}
