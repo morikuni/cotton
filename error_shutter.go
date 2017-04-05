7f8aab91c3e22610fa4da49d3b8327af4dcf94ba
morikuni <taihei.with+git@gmail.com>
2017-04-05 23:34:22 +0900

    Add DefaultErrorShutter

--- error_shutter.go
+++ error_shutter.go
@@ -1,7 +1,12 @@
 package yacm
 
 import (
+	"io"
 	"net/http"
+)
+
+var (
+	DefaultErrorShutter = ErrorShutterFunc(defaultErrorShutter)
 )
 
 type ErrorShutter interface {
@@ -12,4 +17,9 @@ type ErrorShutterFunc func(w http.ResponseWriter, r *http.Request, err error)
 
 func (f ErrorShutterFunc) ShutError(w http.ResponseWriter, r *http.Request, err error) {
 	f(w, r, err)
+}
+
+func defaultErrorShutter(w http.ResponseWriter, _ *http.Request, _ error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	io.WriteString(w, http.StatusText(http.StatusInternalServerError))
 }
